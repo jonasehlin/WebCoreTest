c5bd1d1bb846ce1589bdcd14aff331a6a1629c7d
Jonas Ehlin <jonas.ehlin@joliv.se>
2018-11-14 16:13:53 +0100

    Boy scout

--- WebCoreTest/Startup.cs
+++ WebCoreTest/Startup.cs
@@ -8,12 +8,12 @@ namespace WebCoreTest
 {
 	public class Startup
 	{
+		public IConfiguration Configuration { get; }
+
 		public Startup(IConfiguration configuration)
 		{
 			Configuration = configuration;
 		}
-
-		public IConfiguration Configuration { get; }
 
 		/// <remarks>
 		/// This method gets called by the runtime. Use this method to add services to the container.
